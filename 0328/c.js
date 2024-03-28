64a8f09dcd8759c21364630dc470e8522648a036
ellapierce06 <ella.k.pierce-1@ou.edu>
2024-03-28 10:19:18 -0500

    LC 03 28

---
+++ 0328/c.js
@@ -0,0 +1,72 @@
+"use strict";
+
+let R = null;
+let chart = null;//for char object
+let tbl2 = null;
+
+let b1 = document.querySelector("#b1");
+
+async function sc()
+{
+
+    let url = `https://juxinglong.github.io/static/data/states.json`;
+
+    let r = await fetch( url );//async
+    let rj = await r.json();//async
+
+    //get container
+    let cc = document.querySelector( "#cc" );
+
+    let opts = {
+        type: "bar", // line, bar
+        data: {
+            labels:rj.map(x=>x.st),
+            datasets: [{
+                data: rj.map(x => x.p),
+                label:"Population",
+            },],
+        },
+    };
+
+    if (chart != null)
+    {
+        chart.destroy();
+    }
+    cc.innerHTML = ``;
+
+
+    chart = new Chart(cc, opts);
+
+    //show table
+    //rj
+    let sdiv = document.querySelector("#sdiv");
+
+    let p = {
+        data: rj,
+        pagination: { limit: 5, },
+        sort: true,
+        search: true,
+        columns: [{ id: "st", name: "STATE" }, {id:"p", name:"POP"},],
+
+    };
+
+    if( tbl2!=null )
+    {
+        tbl2.destroy();
+
+    }
+    sdiv.innerHTML = ``;
+
+
+    tbl2 = new gridjs.Grid(p);
+    tbl2.render(sdiv);
+     
+    console.log(rj);
+    R = rj;
+
+    //fetch()
+    //Swal.ire( "OK" );
+
+}
+
+b1.addEventListener("click",sc);
